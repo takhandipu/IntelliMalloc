c480e4208dcd41a3c83593bfeadf7d203ec00fae
Tanvir Ahmed Khan <takhandipu@gmail.com>
2018-04-17 16:31:33 -0400

    Eigen padding experiment is fully completed.

--- tensor/run.sh
+++ tensor/run.sh
@@ -2,6 +2,6 @@ for (( d=1; d<=128; d*=2 ))
 do
 	for (( c=100; c<=2000; c+=100 ))
 	do
-		ocperf.py stat -o $d\_$c.txt -r 1 -e mem_load_uops_l3_hit_retired.xsnp_hitm ./$d\_$c
+		ocperf.py stat -o $d\_$c.txt -r 100 -e mem_load_uops_l3_hit_retired.xsnp_hitm ./$d\_$c
 	done
 done
