a752bd004d52fa07965d82ee7f9126f64146618e
Tanvir Ahmed Khan <takhandipu@gmail.com>
2018-04-21 22:16:46 -0400

    Padding on eigen lock and queue

--- tensor/run.sh
+++ tensor/run.sh
@@ -1,7 +1,4 @@
-for (( d=1; d<=128; d*=2 ))
+for (( c=100; c<=2000; c+=100 ))
 do
-	for (( c=100; c<=2000; c+=100 ))
-	do
-		ocperf.py stat -o $d\_$c.txt -r 100 -e mem_load_uops_l3_hit_retired.xsnp_hitm ./$d\_$c
-	done
+  ocperf.py stat -o $c.txt -r 100 -e mem_load_uops_l3_hit_retired.xsnp_hitm ./$c
 done
